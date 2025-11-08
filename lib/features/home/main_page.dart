5a864495f7caed28f8adf4fd3570e61c23ca733f
Miso <nguyenalex24@gmail.com>
2025-11-08 15:14:06 -0800

    Refactor out the main page into its own widget

---
+++ lib/features/home/main_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:pododoro/features/pododoro_timer.dart';
+import 'package:pododoro/features/timer_page/timer_page.dart';
+import 'package:pododoro/features/timer.dart';
+import 'package:pododoro/constants.dart';
+import 'package:pododoro/main.dart';
+import 'package:isar/isar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  Timer? _activeTimer;
+  final List<Timer> _timers = <Timer>[];
+  int _currentPageIndex = 0;
+  List<Widget> get _pages => [
+    PododoroTimer(timer: _activeTimer),
+    TimerPage(timers: _timers, activeTimerName: _activeTimer?.name, onSelectTimer: _setActiveTimer),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeTimers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(child: Text(_activeTimer?.name ?? "", style: TextStyle(color: Colors.white))),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      backgroundColor: Constants.defaultBackgroundColor,
+      body: _pages[_currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        indicatorColor: const Color.fromARGB(255, 120, 128, 121),
+        destinations: [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.timelapse),
+            label: "Timers",
+          ),
+        ],
+        selectedIndex: _currentPageIndex,
+        onDestinationSelected: (value) => setState(() => _currentPageIndex = value),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+
+  /// Get all existing timers from internal database. If there are no existing timers, then add the default one to the database.
+  Future initializeTimers() async {
+    var existingTimers = await isar.timers.where().findAll();
+
+    if (existingTimers.isEmpty) {
+      // Create the default timer
+      Timer defaultTimer = Timer(name: "Pododoro timer", totalMinutes: 25, totalSeconds: 0);
+      await isar.writeTxn(() async => await isar.timers.put(defaultTimer));
+
+      existingTimers.add(defaultTimer);
+    }
+
+    setState(() {
+      _timers.addAll(existingTimers);
+      if (_timers.isNotEmpty) _activeTimer = _timers[0];
+    });
+  }
+
+  /// Set the active timer.
+  void _setActiveTimer(String? timerName) {
+    if (timerName == null) return;
+
+    setState(() => _activeTimer = _timers.firstWhere((timer) => timer.name == timerName));
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:pododoro/constants.dart';
-import 'package:pododoro/features/pododoro_timer.dart';
+import 'package:pododoro/features/home/main_page.dart';
 import 'package:pododoro/features/timer.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pododoro/features/timer_page/timer_page.dart';
 
 /// Used to handle navigating to the correct page when the app is closed and the local notification is tapped
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -47,93 +45,13 @@ class PododoroTimerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pododoro',
+      title: 'Pododoro timer',
       navigatorKey: navigatorKey,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MainPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Timer? _activeTimer;
-  final List<Timer> _timers = <Timer>[];
-    int _currentPageIndex = 0;
-
-  List<Widget> get _pages => [
-    PododoroTimer(timer: _activeTimer),
-    TimerPage(timers: _timers, activeTimerName: _activeTimer?.name, onSelectTimer: _setActiveTimer),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-
-    initializeUserData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Center(child: Text(_activeTimer?.name ?? "", style: TextStyle(color: Colors.white))),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      backgroundColor: Constants.defaultBackgroundColor,
-      body: _pages[_currentPageIndex],
-      bottomNavigationBar: NavigationBar(
-        indicatorColor: const Color.fromARGB(255, 120, 128, 121),
-        destinations: [
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.timelapse),
-            label: "Timers",
-          ),
-        ],
-        selectedIndex: _currentPageIndex,
-        onDestinationSelected: (value) => setState(() => _currentPageIndex = value),
-        backgroundColor: Colors.black,
-      ),
-    );
-  }
-
-  Future initializeUserData() async {
-    var existingTimers = await isar.timers.where().findAll();
-
-    if (existingTimers.isEmpty) {
-      // Create the default timer
-      Timer defaultTimer = Timer(name: "Pododoro timer", totalMinutes: 25, totalSeconds: 0);
-      await isar.writeTxn(() async => await isar.timers.put(defaultTimer));
-
-      existingTimers.add(defaultTimer);
-    }
-
-    setState(() {
-      _timers.addAll(existingTimers);
-      if (_timers.isNotEmpty) _activeTimer = _timers[0];
-    });
-  }
-
-  void _setActiveTimer(String? timerName) {
-    if (timerName == null) return;
-
-    setState(() {
-      _activeTimer = _timers.firstWhere((timer) => timer.name == timerName);
-    });
   }
 }
